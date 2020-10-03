8017a2886b933496ee5686150edce771f3f097d5
brian fang <brianfang@ucla.edu>
2020-10-03 14:48:44 -0700

    Created camera and color class and tested renderImage method.

--- main.py
+++ main.py
@@ -31,17 +31,32 @@ class Sphere:
     def computeLight(self, ray):
         pass
 
+class Camera:
+    def __init__(self, ray, fov):
+        self.ray = ray
+        self.fov = fov
+
+    def renderImage(self, scene, data, width, height):
+        for x in range(width):
+            for y in range(height):
+                data[x,y] = [x,y,0]
+
+
+class Color:
+    def __init__(self, rgb):
+        self.color = [rgb[0]/255, rgb[1]/255, rgb[2]/255]
+
+    def restore(self):
+        return [self.color[0]*255, self.color[1]*255, self.color[2]*255]
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    data = np.zeros((1024, 1024, 3), dtype = np.uint8)
-
-    for i in range(100):
-        for j in range(100):
-            data[i, j] = [255, 0, 0]
-
-    data[100, 101] = [0, 255, 0]
-    data[101, 100] = [0, 0, 255]
+    width = 1024
+    height = 1024
+    data = np.zeros((width, height, 3), dtype = np.uint8)
+    camera = Camera(Ray([0,0,0], [0,0,1]), np.pi/2)
+    camera.renderImage(Scene(), data, width, height)
 
     image = Image.fromarray(data)
     image.show()
